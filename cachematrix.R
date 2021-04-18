2e844b61ac4380f2c96ab338a7a96472e89b555d
sharinacabreradelacruz <82770681+sharinacabreradelacruz@users.noreply.github.com>
2021-04-19 00:34:31 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+setwd('c:/Users/asus/Downloads')
+##
+## input x as matrix
+## set every solved value "a" as null
+## changed every "mean" to "solve"
+gogoCachematrix <- function(x = matrix(sample(1:500,7),9,8) {
+    a <- NULL
+    usb <- function(y) {
+        x <<- y
+        a <<- NULL
+    }
+    grab <- function() x
+    usbgrab <- function(solve) a <<- inverse
+    usbgrabgo <- function() s
+    list(set = set,
+         get = get,
+         setmean = setmean,
+         getmean = getmean)
 }
 
 
-## Write a short comment describing this function
+## input x as matrix
+## set "a" as null
+## change "mean"to "solve"
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    x <- x$getmean()
+    if (!is.null(a)) {
+        message("getting average data")
+        return(a)
+    }
+    data <- x$get()
+    a <- solve(data, ...)
+    x$setmean(a)
+    v
 }
